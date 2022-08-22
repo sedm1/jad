ac353c438648464789832de071ef481055a3c03f
Николай <novikovn383@gmail.com>
2022-08-22 18:33:52 +0500

    jad

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,19 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/jad/'
-    : '/'
+    : '/',
+    pwa: {
+      name: 'jad',
+      themeColor: '#ffffff',
+      appleMobileWebAppCapable: 'yes',
+      appleMobileWebAppStatusBarStyle: 'black',
+      iconPaths: {
+        faviconSVG: 'img/icons/favicon.svg',
+        favicon32: 'img/icons/favicon.svg',
+        favicon16: 'img/icons/favicon.svg',
+        appleTouchIcon: 'img/icons/icon-152x152.png',
+        maskIcon: 'img/icons/icon-512x512.png',
+        msTileImage: 'img/icons/icon-144x144.png'
+      }
+    }
 }
